96e4ebd739952dbac9b44df2c4ae05c6ae52c362
Stanislav Rogovskyi <stazbass@gmail.com>
2020-05-11 23:26:35 +0300

    step13 init

---
+++ src/main/java/com/stzbzz/top50/steps/Step13.java
@@ -0,0 +1,18 @@
+package com.stzbzz.top50.steps;
+
+import com.stzbzz.top50.Solution;
+
+public class Step13 implements Solution {
+    @Override
+    public String toString() {
+        return "Square root of a number";
+    }
+
+    public double sqrt(double num){
+        return Math.sqrt(num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
